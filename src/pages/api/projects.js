2932aba3ee5e0c8b1526594b3a82ccca0e743b4b
Victor Canas <victorcanasint@gmail.com>
2022-11-11 13:56:57 -0500

    Update projects.js

--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -3,9 +3,9 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'json');
+  const jsonDirectory = path.join(process.cwd(), '/src/json');
   //Read the json data file projects.json
   const projectsData = await fs.readFile(jsonDirectory + '/projects.json', 'utf8');
   //Return the content of the data file in json format
-  res.status(200).json(projectsData);
+  res.status(200).json(JSON.parse(projectsData));
 }
